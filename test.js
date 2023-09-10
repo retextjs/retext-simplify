f9cf1a54aeb4d4d62e56d3e3a9222f6d176f1047
Titus Wormer <tituswormer@gmail.com>
2023-09-10 12:50:34 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {retext} from 'retext'
-import retextSimplify from './index.js'
+import retextSimplify from 'retext-simplify'
 
 test('retextSimplify', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('retext-simplify')).sort(), [
       'default'
     ])
   })
